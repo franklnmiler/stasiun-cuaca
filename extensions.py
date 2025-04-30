ca89b90be0bc019eb815792bb38f152c53533ced
Irsyad <myfrankln@gmail.com>
2025-04-30 20:14:11 +0700

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,13 @@
+# extensions.py
+# Diisi saat inisialisasi di app.py
+mysql = None
+firebase_ref = None
+socketio = None
+
+# Optional: fungsi importable umum juga bisa di sini
+send_data = None
+# extensions.py
+mysql = None
+firebase_ref = None
+socketio = None
+send_data = None
